4124b532b025c4bda79b81a3dff204ea5eaebd3d
torvalds <deandb2015@gmail.com>
2019-03-27 18:05:49 +0800

    Add the admin and users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  unless User.exists?(admin: true)
+    p '---------- New a admin ----------'
+    FactoryGirl.create(:admin, email: "admin@gmail.com", password: "ekoheadmin123")
+    p '---------- Congratulations! Created an admin. ----------'
+  end
+
+  p '---------- New some users ----------'
+  FactoryGirl.create_list(:user, 50, password: "ekohe123")
+  p '---------- Congratulations! Created 50 users. ----------'
+end
