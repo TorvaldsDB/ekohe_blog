df88ae412a4a1bd43c035adbc95fc3efe5559c01
torvalds <deandb2015@gmail.com>
2019-03-27 19:02:39 +0800

    Add user validations and rspec test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+
   has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6, maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
