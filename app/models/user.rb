b089a0c934fcba10c2d8c32ac8d8177db96b31bf
Torvalds Du <torvalds@ekohe.com>
2020-12-24 16:32:51 +0800

    Change email regexp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   before_save { self.email = email.downcase }
 
   has_secure_password
@@ -10,6 +8,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :password, presence: true, length: { minimum: 6, maximum: 255 }
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
 end
